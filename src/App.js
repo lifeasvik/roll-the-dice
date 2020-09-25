249ff37eb597daa698471d592b1db0f247ff7d62
Vik Birdi <lifeasvik@gmail.com>
2020-09-24 20:20:57 -0500

    finished game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Game } from "./Game";
+import { Interface } from "./Interface";
+import { AIresult } from "./AIresult";
+import { YourResult } from "./YourResult";
+import { Ranking } from "./Ranking";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  draws = [];
+  losses = [];
+  wins = [];
+  state = {
+    games: 0,
+    yourChoice: "",
+    AIChoice: null,
+    one:
+      "https://game-icons.net/icons/ffffff/000000/1x1/delapouite/dice-six-faces-one.svg",
+    two:
+      "https://game-icons.net/icons/ffffff/000000/1x1/delapouite/dice-six-faces-two.svg",
+    three:
+      "https://game-icons.net/icons/ffffff/000000/1x1/delapouite/dice-six-faces-three.svg",
+    four:
+      "https://game-icons.net/icons/ffffff/000000/1x1/delapouite/dice-six-faces-four.svg",
+    five:
+      "https://game-icons.net/icons/ffffff/000000/1x1/delapouite/dice-six-faces-five.svg",
+    six:
+      "https://game-icons.net/icons/ffffff/000000/1x1/delapouite/dice-six-faces-six.svg"
+  };
+  AIChoice = () => {
+    this.setState({
+      AIChoice: Math.floor(Math.random() * 6) + 1
+    });
+  };
+
+  startGame = () => {
+    this.setState({
+      yourChoice: Math.floor(Math.random() * 6) + 1,
+      games: this.state.games + 1
+    });
+
+    this.AIChoice();
+  };
+
+  clearState = () => {
+    this.losses = [];
+    this.wins = [];
+    this.draws = [];
+    this.setState({
+      yourChoice: "",
+      AIChoice: null,
+      games: 0
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Interface
+          games={this.state.games}
+          clearState={this.clearState}
+          startGame={this.startGame}
+          yourChoice={this.state.yourChoice}
+          AIChoice={this.state.AIChoice}
+        />
+        <div className="effects">
+          <YourResult allstates={this.state} />
+          <AIresult allstates={this.state} />
+        </div>
+        <Game results={this.state} />
+        <Ranking
+          wins={this.wins}
+          losses={this.losses}
+          draws={this.draws}
+          yourChoice={this.state.yourChoice}
+          AIChoice={this.state.AIChoice}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
